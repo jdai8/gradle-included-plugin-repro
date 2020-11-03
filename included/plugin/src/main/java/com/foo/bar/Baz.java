49a7eb9b6b5c929abc5df32a757d6d88e5930e98
Jack Dai <jdai88@gmail.com>
2020-11-03 12:00:37 -0500

    reproduce

---
+++ included/plugin/src/main/java/com/foo/bar/Baz.java
@@ -0,0 +1,10 @@
+package com.foo.bar;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+
+public class Baz implements Plugin<Project> {
+    public void apply(Project project) {
+    }
+}
